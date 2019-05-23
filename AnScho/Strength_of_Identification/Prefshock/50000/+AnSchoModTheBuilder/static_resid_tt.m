8055dd4bda254d1af0e64d63cf46f7e3411c3de7
Willi Mutschler <willi@mutschler.eu>
2019-05-23 09:43:28 +0200

    Added AnScho Identification Strength Prefshock

---
+++ AnScho/Strength_of_Identification/Prefshock/50000/+AnSchoModTheBuilder/static_resid_tt.m
@@ -0,0 +1,37 @@
+function T = static_resid_tt(T, y, x, params)
+% function T = static_resid_tt(T, y, x, params)
+%
+% File created by Dynare Preprocessor from .mod file
+%
+% Inputs:
+%   T         [#temp variables by 1]  double   vector of temporary terms to be filled by function
+%   y         [M_.endo_nbr by 1]      double   vector of endogenous variables in declaration order
+%   x         [M_.exo_nbr by 1]       double   vector of exogenous variables in declaration order
+%   params    [M_.param_nbr by 1]     double   vector of parameter values in declaration order
+%
+% Output:
+%   T         [#temp variables by 1]  double   vector of temporary terms
+%
+
+assert(length(T) >= 18);
+
+T(1) = 1+params(2)/400;
+T(2) = (1-params(5))^(1/params(4))*y(8);
+T(3) = y(5)^(-params(4));
+T(4) = y(5)^(-params(4));
+T(5) = y(10);
+T(6) = y(10)*T(3);
+T(7) = 1/(1+params(1)/400);
+T(8) = T(4)*T(5);
+T(9) = 1+params(3)/100;
+T(10) = (y(7));
+T(11) = (y(7));
+T(12) = (y(6))*(y(7)/T(1))^params(6)*(y(4)/T(2))^params(7);
+T(13) = 1/params(15);
+T(14) = (y(7)-T(11))*y(4)*T(8)/T(6)/y(4);
+T(15) = params(14)/2*(y(7)-T(10))^2;
+T(16) = T(12)^(1-params(8));
+T(17) = y(6)^params(8);
+T(18) = exp(params(11)/100*x(1));
+
+end
