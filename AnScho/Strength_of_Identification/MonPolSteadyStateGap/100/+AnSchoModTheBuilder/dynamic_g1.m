ce34052f3184b466e3d7f36c11412389601de102
Willi Mutschler <willi@mutschler.eu>
2019-05-23 09:47:51 +0200

    Added AnScho Identification Strength MonPolSteadyStateGap

---
+++ AnScho/Strength_of_Identification/MonPolSteadyStateGap/100/+AnSchoModTheBuilder/dynamic_g1.m
@@ -0,0 +1,61 @@
+function g1 = dynamic_g1(T, y, x, params, steady_state, it_, T_flag)
+% function g1 = dynamic_g1(T, y, x, params, steady_state, it_, T_flag)
+%
+% File created by Dynare Preprocessor from .mod file
+%
+% Inputs:
+%   T             [#temp variables by 1]     double   vector of temporary terms to be filled by function
+%   y             [#dynamic variables by 1]  double   vector of endogenous variables in the order stored
+%                                                     in M_.lead_lag_incidence; see the Manual
+%   x             [nperiods by M_.exo_nbr]   double   matrix of exogenous variables (in declaration order)
+%                                                     for all simulation periods
+%   steady_state  [M_.endo_nbr by 1]         double   vector of steady state values
+%   params        [M_.param_nbr by 1]        double   vector of parameter values in declaration order
+%   it_           scalar                     double   time period for exogenous variables for which
+%                                                     to evaluate the model
+%   T_flag        boolean                    boolean  flag saying whether or not to calculate temporary terms
+%
+% Output:
+%   g1
+%
+
+if T_flag
+    T = AnSchoModTheBuilder.dynamic_g1_tt(T, y, x, params, steady_state, it_);
+end
+g1 = zeros(9, 20);
+g1(1,9)=T(35);
+g1(1,15)=(-(y(10)*T(7)*T(39)/y(16)/(T(9)*y(17))));
+g1(1,10)=(-(T(7)*T(8)/y(16)/(T(9)*y(17))));
+g1(1,16)=(-((-(y(10)*T(7)*T(8)))/(y(16)*y(16))/(T(9)*y(17))));
+g1(1,17)=(-((-(y(10)*T(7)*T(8)/y(16)*T(9)))/(T(9)*y(17)*T(9)*y(17))));
+g1(2,8)=(-(T(7)*params(14)*y(16)*T(29)));
+g1(2,14)=(-(T(7)*params(14)*y(16)*T(34)));
+g1(2,9)=(-(1/params(5)*(-(T(35)*T(36)))+T(7)*params(14)*y(16)*T(38)));
+g1(2,15)=(-(T(7)*params(14)*y(16)*T(41)));
+g1(2,11)=(-(params(14)*(y(11)-T(10))+y(11)*params(14)-params(14)/(2*params(5))*2*(y(11)-T(11))));
+g1(2,16)=(-(T(7)*params(14)*(T(16)+y(16)*T(15))));
+g1(3,8)=1-T(17)-(1-1/y(12));
+g1(3,9)=(-1);
+g1(3,11)=(-(y(8)*params(14)/2*2*(y(11)-T(10))));
+g1(3,12)=(-(y(8)*(-((-1)/(y(12)*y(12))))));
+g1(4,8)=(-(T(20)*T(19)*T(31)*T(32)));
+g1(4,2)=(-(T(20)*T(18)*T(42)));
+g1(4,10)=1;
+g1(4,11)=(-(T(20)*T(19)*T(32)*T(44)));
+g1(4,18)=(-(T(18)*T(19)*T(45)));
+g1(5,3)=(-(params(9)*1/y(3)));
+g1(5,12)=1/y(12);
+g1(5,19)=(-(params(12)/100));
+g1(6,4)=(-(params(10)*1/y(4)));
+g1(6,13)=1/y(13);
+g1(6,20)=(-(params(13)/100));
+g1(7,5)=1;
+g1(7,1)=(-(100*(-(T(27)/T(21)))));
+g1(7,8)=(-(100*T(27)/T(14)));
+g1(7,13)=(-(100*1/(steady_state(9))/T(22)));
+g1(8,6)=1;
+g1(8,11)=(-(400*1/(steady_state(7))/T(23)));
+g1(9,7)=1;
+g1(9,10)=(-(400*1/(steady_state(6))/T(24)));
+
+end
---
+++ AnScho/Strength_of_Identification/MonPolSteadyStateGap/50000/+AnSchoModTheBuilder/static_g1_tt.m
@@ -0,0 +1,25 @@
+function T = static_g1_tt(T, y, x, params)
+% function T = static_g1_tt(T, y, x, params)
+%
+% File created by Dynare Preprocessor from .mod file
+%
+% Inputs:
+%   T         [#temp variables by 1]  double   vector of temporary terms to be filled by function
+%   y         [M_.endo_nbr by 1]      double   vector of endogenous variables in declaration order
+%   x         [M_.exo_nbr by 1]       double   vector of exogenous variables in declaration order
+%   params    [M_.param_nbr by 1]     double   vector of parameter values in declaration order
+%
+% Output:
+%   T         [#temp variables by 1]  double   vector of temporary terms
+%
+
+assert(length(T) >= 22);
+
+T = AnSchoModTheBuilder.static_resid_tt(T, y, x, params);
+
+T(19) = (y(7)/T(1))^params(6);
+T(20) = (y(4)/(y(4)))^params(7);
+T(21) = getPowerDeriv(T(12),1-params(8),1);
+T(22) = getPowerDeriv(y(5),(-params(4)),1);
+
+end
